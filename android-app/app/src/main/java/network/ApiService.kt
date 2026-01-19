3c50d7c99a29c08623d11939008856d47952306d
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-01-19 16:34:33 -0300

    feat: define interface de serviço da api

---
+++ android-app/app/src/main/java/network/ApiService.kt
@@ -0,0 +1,8 @@
+package network
+import com.example.portable.FichaResumo
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.GET
+
+interface ApiService {}
