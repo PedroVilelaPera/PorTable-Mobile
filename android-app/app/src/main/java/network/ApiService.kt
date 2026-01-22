69cbc47faf56f5b29201bc549d436ca6d7575ffb
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-01-22 20:15:29 -0300

    feat: implementa camada de rede com Retrofit

--- android-app/app/src/main/java/network/ApiService.kt
+++ android-app/app/src/main/java/network/ApiService.kt
@@ -1,8 +1,11 @@
 package network
-import com.example.portable.FichaResumo
+import com.example.portable.model.LoginRequest
+import com.example.portable.model.LoginResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.GET
 
-interface ApiService {}
+interface ApiService {
+    @POST("auth/login")
+    fun login(@Body request: LoginRequest): Call<LoginResponse>
+}
