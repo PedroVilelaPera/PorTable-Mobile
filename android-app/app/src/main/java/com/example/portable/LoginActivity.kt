77ed68aa929cb70312903c7f9c0b07201076cf6f
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-01-22 20:25:34 -0300

    feat: integra lógica de login com retrofit

--- android-app/app/src/main/java/com/example/portable/LoginActivity.kt
+++ android-app/app/src/main/java/com/example/portable/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.portable
-
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.portable.databinding.ActivityLoginBinding
+import com.example.portable.model.LoginRequest
+import com.example.portable.model.LoginResponse
+import network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.jvm.java
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -58,19 +68,35 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun executarNavegacaoLogin() {
+        val email = binding.edittextInputEmail.text.toString()
+        val senha = binding.edittextInputSenha.text.toString()
 
-        // TODO: BUSCAR FICHAS NO BD
+        val req = LoginRequest(email,senha)
 
-        val listaMock = arrayListOf( // PLACEHOLDER (USE FichaResumo)
-            FichaResumo(1, "ABEL"),
-            FichaResumo(2, "BELA"),
-            FichaResumo(3, "CAIN")
-        )
+        RetrofitClient.instance.login(req).enqueue(object : Callback<LoginResponse>{
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                if (response.isSuccessful) {
+                    val resBody = response.body()
 
-        val intent = Intent(this, ListaDeFichasActivity::class.java)
-        intent.putParcelableArrayListExtra("LISTA_FICHAS", listaMock)
-        startActivity(intent)
-        finish()
+                    if (resBody?.status == true) {
+                        val intent = Intent(this@LoginActivity, ListaDeFichasActivity::class.java)
+                        intent.putExtra("USER_ID", response.body()?.userId)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // Erro de credênciais
+                        exibirDialogLogout("!!! ERRO !!!", "E-mail ou senha incorretos.")
+                    }
+                } else {
+                    // Erro de Servidor (404 ou 500)
+                    exibirDialogLogout("ERRO NO SERVIDOR", "Ocorreu um problema técnico. Tente mais tarde.")
+                }
+            }
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                // Erro de conexão (Ip errado ou sem internet)
+                exibirDialogLogout("ERRO NA CONEXÃO", "Não foi possível alcançar o servidor. Verifique sua rede.")
+            }
+        })
     }
 
     private fun configurarAutoScroll() {
@@ -84,4 +110,30 @@ class LoginActivity : AppCompatActivity() {
         binding.edittextInputEmail.setOnFocusChangeListener(listener)
         binding.edittextInputSenha.setOnFocusChangeListener(listener)
     }
+
+    private fun exibirDialogLogout(title: String, message: String) {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_custom, null)
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        dialogView.findViewById<TextView>(R.id.dialog_title).text = title
+        dialogView.findViewById<TextView>(R.id.dialog_message).text = message
+
+        // Esconde botão cancelar
+        val btnCancel = dialogView.findViewById<TextView>(R.id.btn_cancel)
+        btnCancel.visibility = View.GONE
+
+        // Modifica botão de confirmação para fechar o pop-up
+        val btnConfirm = dialogView.findViewById<TextView>(R.id.btn_confirm)
+        btnConfirm.text = "[ OK ]"
+        btnConfirm.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
 }
