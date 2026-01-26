4f41d7dccfe78822771be616025eaceaf349b1f7
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-01-25 21:54:25 -0300

    feat: adiciona uma variação do popup de alerta voltada para erros

--- android-app/app/src/main/java/com/example/portable/ViewExtension.kt
+++ android-app/app/src/main/java/com/example/portable/ViewExtension.kt
@@ -1,9 +1,14 @@
 package com.example.portable
 
+import android.R.id.message
 import android.content.Context
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 fun EditText.configurarEnterParaTirarFoco() {
     this.setOnEditorActionListener { v, actionId, _ ->
@@ -23,4 +28,29 @@ fun EditText.configurarEnterParaTirarFoco() {
 
 fun String.limparEspacosExtras(): String {
     return this.trim().replace("\\s+".toRegex(), " ")
+}
+
+fun AppCompatActivity.exibirAvisoErro(titulo: String, mensagem: String) {
+    val dialogView = layoutInflater.inflate(R.layout.dialog_custom, null)
+    val dialog = AlertDialog.Builder(this)
+        .setView(dialogView)
+        .create()
+
+    dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+    dialogView.findViewById<TextView>(R.id.dialog_title).text = titulo
+    dialogView.findViewById<TextView>(R.id.dialog_message).text = mensagem
+
+    // Esconde botão cancelar
+    val btnCancel = dialogView.findViewById<TextView>(R.id.btn_cancel)
+    btnCancel.visibility = View.GONE
+
+    // Modifica botão de confirmação para fechar o pop-up
+    val btnConfirm = dialogView.findViewById<TextView>(R.id.btn_confirm)
+    btnConfirm.text = "[ OK ]"
+    btnConfirm.setOnClickListener {
+        dialog.dismiss()
+    }
+
+    dialog.show()
 }
