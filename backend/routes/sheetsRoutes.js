f3aefcffdeae4ba8eab623ba23bc49aaac970906
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-01-26 20:43:38 -0300

    feat: adiciona rota de criação, remoção e busca das fichas do usuário

--- backend/routes/sheetsRoutes.js
+++ backend/routes/sheetsRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getSheetsByUser } from '../controllers/sheetController.js';
+import { getSheetsByUser, createSheet, deleteSheet , getSheetById } from '../controllers/sheetController.js';
 
 const router = express.Router();
 
 router.get('/:usuario_id', getSheetsByUser);
+router.post('/', createSheet);             
+router.delete('/:id', deleteSheet);
+router.get('/id/:id', getSheetById);
 
 export default router;
