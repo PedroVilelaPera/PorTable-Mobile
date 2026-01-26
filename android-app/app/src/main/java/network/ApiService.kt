8b6f0abd31b9cb172acc15ced96fdc65414708ec
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-01-26 20:46:50 -0300

    feat: implementa rotas de criação, remoção e busca das fichas do usuário ao apiservice

--- android-app/app/src/main/java/network/ApiService.kt
+++ android-app/app/src/main/java/network/ApiService.kt
@@ -4,6 +4,7 @@ import com.example.portable.model.LoginRequest
 import com.example.portable.model.LoginResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -14,4 +15,13 @@ interface ApiService {
 
     @GET("fichas/{usuario_id}")
     fun getSheets(@Path("usuario_id") usuarioId: Int): Call<List<FichaResponse>>
+
+    @POST("fichas")
+    fun createSheet(@Body request: FichaResponse): Call<FichaResponse>
+
+    @DELETE("fichas/{id}")
+    fun deleteSheet(@Path("id") id: Int): Call<Void> // Call<Void> por não esperar retorno.
+
+    @GET("fichas/id/{id}")
+    fun getSheet(@Path("id") id: Int): Call<FichaResponse>
 }
