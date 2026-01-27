2e4337422d504e3dc2e57906486d7b50f810c2c4
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-01-27 17:32:58 -0300

    feat: adiciona rota de salvamento de fichas ao apiservice

--- android-app/app/src/main/java/network/ApiService.kt
+++ android-app/app/src/main/java/network/ApiService.kt
@@ -7,6 +7,7 @@ import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface ApiService {
@@ -24,4 +25,7 @@ interface ApiService {
 
     @GET("fichas/id/{id}")
     fun getSheet(@Path("id") id: Int): Call<FichaResponse>
+
+    @PUT("fichas/{id}")
+    fun updateSheet(@Path("id") id: Int, @Body request: FichaResponse): Call<Void>
 }
