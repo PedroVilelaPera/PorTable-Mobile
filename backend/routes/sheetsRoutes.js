db61744e32354aeaf07e9e4f3af56e0e74bbbac3
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2026-01-27 17:35:56 -0300

    feat: adiciona rota de salvamento de fichas.

--- backend/routes/sheetsRoutes.js
+++ backend/routes/sheetsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getSheetsByUser, createSheet, deleteSheet , getSheetById } from '../controllers/sheetController.js';
+import { getSheetsByUser, createSheet, deleteSheet , getSheetById , updateSheet} from '../controllers/sheetController.js';
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get('/:usuario_id', getSheetsByUser);
 router.post('/', createSheet);             
 router.delete('/:id', deleteSheet);
 router.get('/id/:id', getSheetById);
+router.put('/:id', updateSheet);
 
 export default router;
